0c457ef0197c1d0825fbb821ef05ae38f63e43d1
Anna Roberts <annaroberts960608@gmail.com>
2015-12-08 11:23:38 -0500

    add jQuery plug in for acordian menu and add content for menu

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $(".accordion").accordion({
+        firstChildExpand: true,
+        multiExpand: false,
+        slideSpeed: 500,
+        dropDownIcon: "&#9660",
+    });
+
+});
+
+
