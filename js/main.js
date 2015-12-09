5a148c3779db7fbdc9d771332a6b4d001dfd126b
Anna Roberts <annaroberts960608@gmail.com>
2015-12-09 12:40:48 -0500

    remove unneeded java script

--- js/main.js
+++ js/main.js
@@ -10,87 +10,26 @@ $(document).ready(function() {
 
 $(function(){
 
-
     $("#slides").slidesjs({
-        width: 940,
-        height: 528
-    });
-  $("#slides").slidesjs({
     navigation: {
       active: true,
       effect: "slide"
-    }
-  });
-});
-
-$(function(){
-  $("#slides").slidesjs({
-    effect: {
-      slide: {
-        speed: 200
-      },
-      fade: {
-        speed: 300,
-        crossfade: true
       }
-    }
-  });
-});
-
-$(function(){
-
+    });
 
     $("#slides-beer").slidesjs({
-        width: 940,
-        height: 528
-    });
-  $("#slides-beer").slidesjs({
     navigation: {
       active: true,
       effect: "slide"
-    }
-  });
-});
-
-$(function(){
-  $("#slides-beer").slidesjs({
-    effect: {
-      slide: {
-        speed: 200
-      },
-      fade: {
-        speed: 300,
-        crossfade: true
       }
-    }
-  });
-});
-
-$(function(){
-
+     });
 
     $("#slides-dining").slidesjs({
-        width: 940,
-        height: 528
-    });
-  $("#slides-beer").slidesjs({
     navigation: {
       active: true,
       effect: "slide"
     }
   });
+
 });
 
-$(function(){
-  $("#slides-dining").slidesjs({
-    effect: {
-      slide: {
-        speed: 200
-      },
-      fade: {
-        speed: 300,
-        crossfade: true
-      }
-    }
-  });
-});
