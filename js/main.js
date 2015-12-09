a6d646a839de0209947b7410c417cfb3948fa9b5
Anna Roberts <annaroberts960608@gmail.com>
2015-12-09 01:09:19 -0500

    add slide show and adjust placement on index, beer, and dining pages

--- js/main.js
+++ js/main.js
@@ -6,7 +6,91 @@ $(document).ready(function() {
         slideSpeed: 500,
         dropDownIcon: "&#9660",
     });
-
 });
 
+$(function(){
 
+
+    $("#slides").slidesjs({
+        width: 940,
+        height: 528
+    });
+  $("#slides").slidesjs({
+    navigation: {
+      active: true,
+      effect: "slide"
+    }
+  });
+});
+
+$(function(){
+  $("#slides").slidesjs({
+    effect: {
+      slide: {
+        speed: 200
+      },
+      fade: {
+        speed: 300,
+        crossfade: true
+      }
+    }
+  });
+});
+
+$(function(){
+
+
+    $("#slides-beer").slidesjs({
+        width: 940,
+        height: 528
+    });
+  $("#slides-beer").slidesjs({
+    navigation: {
+      active: true,
+      effect: "slide"
+    }
+  });
+});
+
+$(function(){
+  $("#slides-beer").slidesjs({
+    effect: {
+      slide: {
+        speed: 200
+      },
+      fade: {
+        speed: 300,
+        crossfade: true
+      }
+    }
+  });
+});
+
+$(function(){
+
+
+    $("#slides-dining").slidesjs({
+        width: 940,
+        height: 528
+    });
+  $("#slides-beer").slidesjs({
+    navigation: {
+      active: true,
+      effect: "slide"
+    }
+  });
+});
+
+$(function(){
+  $("#slides-dining").slidesjs({
+    effect: {
+      slide: {
+        speed: 200
+      },
+      fade: {
+        speed: 300,
+        crossfade: true
+      }
+    }
+  });
+});
